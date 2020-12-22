fafa124be0a1d9b4dc633b4ea0af7a0f2a4cac89
roufy235 <roufy235@gmail.com>
2020-12-22 19:56:47 +0100

    Initial Commit

---
+++ MySampleApp/MySampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySampleAppApp.swift
+//  MySampleApp
+//
+//  Created by Bello Abdulrouf Olatunbosun on 22/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct MySampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
