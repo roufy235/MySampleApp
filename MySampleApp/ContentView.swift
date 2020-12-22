4c76b4d0265bc03392d2129cf265e4e61215ec43
roufy235 <roufy235@gmail.com>
2020-12-22 21:09:44 +0100

    Image(), Text() & Spacer()

--- MySampleApp/ContentView.swift
+++ MySampleApp/ContentView.swift
@@ -9,8 +9,16 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        VStack {
+            Image("logo")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+            
+            Spacer()
+            
+            Text("Hello")
+        }
     }
 }
 
