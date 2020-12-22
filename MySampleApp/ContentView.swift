80601d7e29590e9639483615ac037603998c35ab
roufy235 <roufy235@gmail.com>
2020-12-22 21:57:53 +0100

    War Card Game UI Design

--- MySampleApp/ContentView.swift
+++ MySampleApp/ContentView.swift
@@ -10,14 +10,62 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         
-        VStack {
-            Image("logo")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
+        ZStack {
+            Image("background")
+                .ignoresSafeArea()
             
-            Spacer()
-            
-            Text("Hello")
+            VStack {
+                Spacer()
+                
+                Image("logo")
+                Spacer()
+                HStack {
+                    Spacer()
+                    Image("card2")
+                    Spacer()
+                    Image("card3")
+                    Spacer()
+                }
+                
+                Spacer()
+                
+                Image("dealbutton")
+                
+                Spacer()
+                
+                HStack {
+                    Spacer()
+                    VStack {
+                        Text("Player")
+                            .bold()
+                            .font(.headline)
+                            .foregroundColor(Color.white)
+                            .padding(.bottom, 10.0)
+                    
+                        Text("0")
+                            .font(.largeTitle)
+                            .foregroundColor(Color.white)
+                    }
+                    
+                    Spacer()
+                    
+                    VStack {
+                        Text("CPU")
+                            .bold()
+                            .font(.headline)
+                            .foregroundColor(Color.white)
+                            .padding(.bottom, 10.0)
+                        
+                        Text("0")
+                            .font(.largeTitle)
+                            .foregroundColor(Color.white)
+                    }
+                    
+                    Spacer()
+                }
+                
+                Spacer()
+            }
         }
     }
 }
